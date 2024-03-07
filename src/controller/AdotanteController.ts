1fcf032003a4451b81d18ae135ae164c6ee3fe16
Bowie <carlarhentschel@gmail.com>
2024-03-07 16:18:18 -0300

    ajusta retorno de erros de uma vez só

--- src/controller/AdotanteController.ts
+++ src/controller/AdotanteController.ts
@@ -7,11 +7,33 @@ import { TipoRequestBodyAdotante, TipoRequestParamsAdotante, TipoResponseBodyAdo
 
 const adotanteBodyValidator: yup.ObjectSchema<Omit<TipoRequestBodyAdotante, "endereco">> = 
   yup.object({
-    nome: yup.string().defined().required(),
+    nome: yup.string().defined().required("O nome é obrigatório."),
     celular: yup.string().defined().required(),
     senha: yup.string().defined().required().min(6),
     foto: yup.string().optional(),
   });
+  /* o omit foi usado aqui e não no arquivo 'tipoAdotante.ts'(linha 3), pois para criar o 
+  adotante, queremos dar a possibilidade pro usuário preencher o endereço (apesar de ser 
+  opcional) */
+  /* 
+    Exemplo:
+    {
+      "nome": "Carla",
+      "celular": "999998888",
+      "senha": "123456",
+      "foto": "foto.jpg",
+      "endereco": "Rua A, 12"
+    }
+
+    {
+                                  -> null?
+      "celular": "",              -> string vazia?
+      "senha": ,                  -> undefined?
+      "foto": "foto.jpg",
+      "endereco": "Rua A, 12"
+    }
+  */
+
 export default class AdotanteController {
     constructor(private repository: AdotanteRepository) {}
 
@@ -25,10 +47,19 @@ export default class AdotanteController {
         let bodyValidated: TipoRequestBodyAdotante;
 
         try {
-          bodyValidated = await adotanteBodyValidator.validate(req.body)
+          bodyValidated = await adotanteBodyValidator.validate(req.body, {
+            abortEarly: false
+          })
         } catch (error) {
           const yupErrors = error as yup.ValidationError;
-          return res.status(400).json({ error: yupErrors.message });
+          const validationErrors: Record<string, string> = {};
+
+          yupErrors.inner.forEach((error) => {
+            if(!error.path) return;
+            validationErrors[error.path] = error.message;
+          })
+
+          return res.status(400).json({ error: validationErrors });
         }
     
         const novoAdotante = new AdotanteEntity(
