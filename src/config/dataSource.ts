dfa8e6303c04be27f30191be0052e556f78ab73b
Bowie <carlarhentschel@gmail.com>
2024-03-08 14:49:15 -0300

    insere demais entidades

--- src/config/dataSource.ts
+++ src/config/dataSource.ts
@@ -1,10 +1,13 @@
 import { DataSource } from "typeorm";
 import PetEntity from "../entities/PetEntity";
+import EnderecoEntity from "../entities/EnderecoEntity";
+import AbrigoEntity from "../entities/AbrigoEntity";
+import AdotanteEntity from "../entities/AdotanteEntity";
 
 export const AppDataSource = new DataSource({
     type: "sqlite",
     database: "./src/config/database.sqlite",
-    entities: [PetEntity],
+    entities: [PetEntity, EnderecoEntity, AbrigoEntity, AdotanteEntity],
     synchronize: true,
 
 })
