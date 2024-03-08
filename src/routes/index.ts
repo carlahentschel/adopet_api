1a0108bc63957a9d54dbb9ea3433e63a82edb2c4
Bowie <carlarhentschel@gmail.com>
2024-03-08 14:55:06 -0300

    refatora rota de pets

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,7 @@ import petRouter from "../routes/petRouter";
 import adotanteRouter from "./adotanteRouter";
 
 const router = (app: express.Router) => {
-    app.use("pets", petRouter);
+    app.use("/pets", petRouter);
     app.use("/adotantes", adotanteRouter);
 };
 
