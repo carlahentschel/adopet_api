03c9930a8739357f134089f693c93db5aa1fdb99
Bowie <carlarhentschel@gmail.com>
2024-03-11 15:01:03 -0300

    refatora interfaces de pet e adotante

--- src/repositories/interfaces/InterfacePetRepository.ts
+++ src/repositories/interfaces/InterfacePetRepository.ts
@@ -1,13 +1,13 @@
 import PetEntity from "../../entities/PetEntity";
 
 export default interface InterfacePetRepository {
-    criaPet(pet:PetEntity): Promise<void>;
-    listaPet(): Promise<PetEntity[]>;
-    atualizaPet(id: number, pet: PetEntity): Promise<{ success: boolean; message?: string }>;
-    deletaPet(id: number): Promise<{ success: boolean; message?: string }>;
-    adotaPet(idPet: number, idAdotante: number): Promise<{ success: boolean; message?: string }>;
+    criaPet(pet:PetEntity): void | Promise<void>;
+    listaPet(): Array<PetEntity> | Promise<PetEntity[]>;
+    atualizaPet(id: number, pet: PetEntity): void;
+    deletaPet(id: number): void;
+    adotaPet(idPet: number, idAdotante: number): void;
     buscaPetPorCampoGenerico<Tipo extends keyof PetEntity>(
         campo: Tipo, 
         valor: PetEntity[Tipo]
-    ): Promise<PetEntity[]>;
+    ): Promise<PetEntity[]> | PetEntity[];
 }
