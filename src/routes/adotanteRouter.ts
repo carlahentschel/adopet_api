be27937d9ed32dbfb6b716b7e0c13929e2674c31
Bowie <carlarhentschel@gmail.com>
2024-03-11 14:59:40 -0300

    adiciona veridicação de ID válido nas rotas de pet e adotante

--- src/routes/adotanteRouter.ts
+++ src/routes/adotanteRouter.ts
@@ -4,6 +4,7 @@ import AdotanteRepository from "../repositories/AdotanteRepository";
 import { AppDataSource } from "../config/dataSource";
 import { middlewareValidadorBodyAdotante } from "../middleware/validadores/adotanteRequestBody";
 import { middlewareValidadorBodyEndereco } from "../middleware/validadores/enderecoRequestBody";
+import { verificaIdMiddleware } from "../middleware/verificaId";
 
 const router = express.Router();
 const adotanteRepository = new AdotanteRepository(
@@ -13,13 +14,19 @@ const adotanteController = new AdotanteController(adotanteRepository);
 
 const validateBodyAdotante: RequestHandler = (req, res, next) => 
   middlewareValidadorBodyAdotante(req, res, next);
-  const validateBodyEndereco: RequestHandler = (req, res, next) => 
+const validateBodyEndereco: RequestHandler = (req, res, next) => 
   middlewareValidadorBodyEndereco(req, res, next);
 
-router.post("/", validateBodyAdotante, adotanteController.criaAdotante); // Rota para criar um adotante
-router.get("/", adotanteController.listaAdotantes);
-router.put("/:id", adotanteController.atualizaAdotante);
-router.delete("/:id", adotanteController.deletaAdotante);
-router.patch("/:id", validateBodyEndereco, adotanteController.atualizaEnderecoAdotante);
+router.post("/", validateBodyAdotante, (req, res) =>
+  adotanteController.criaAdotante(req, res)
+);
+router.get("/", (req, res) => adotanteController.listaAdotantes(req, res));
+router.put("/:id", verificaIdMiddleware, (req, res) => adotanteController.atualizaAdotante(req, res));
+router.delete("/:id", verificaIdMiddleware, (req, res) =>
+  adotanteController.deletaAdotante(req, res)
+);
+router.patch("/:id", verificaIdMiddleware, validateBodyEndereco, (req, res) =>
+  adotanteController.atualizaEnderecoAdotante(req, res)
+);
 
 export default router;
--- src/routes/petRouter.ts
+++ src/routes/petRouter.ts
@@ -4,6 +4,7 @@ import PetRepository from "../repositories/PetRepository";
 import { AppDataSource } from "../config/dataSource";
 import { Request, ParamsDictionary, Response, NextFunction } from "express-serve-static-core";
 import { ParsedQs } from "qs";
+import { verificaIdMiddleware } from "../middleware/verificaId";
 
 const router = express.Router();
 const petRepository = new PetRepository(
@@ -17,9 +18,9 @@ const validateBodyPet: RequestHandler = (req, res, next) =>
 
 router.post("/", validateBodyPet, (req, res)=>petController.criaPet(req, res));
 router.get("/", (req, res)=>petController.listaPets(req, res));
-router.put("/", (req, res)=>petController.atualizaPet(req, res));
-router.delete("/", (req, res)=>petController.deletaPet(req, res));
-router.put("/:pet_id/:adotante_id", (req, res) =>
+router.put("/:id", verificaIdMiddleware,  (req, res)=>petController.atualizaPet(req, res));
+router.delete("/:id", verificaIdMiddleware, (req, res)=>petController.deletaPet(req, res));
+router.put("/:pet_id/:adotante_id", verificaIdMiddleware, (req, res) =>
   petController.adotaPet(req, res)
 );
 router.get("/filtro", (req, res) =>
