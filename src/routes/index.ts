492b1827d76cb08cbd0e3d22626b030f84717092
Bowie <carlarhentschel@gmail.com>
2024-03-11 15:00:26 -0300

    adiciona rota de abrigo

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import petRouter from "../routes/petRouter";
 import adotanteRouter from "./adotanteRouter";
+import abrigoRouter from "./abrigoRouter";
 
 const router = (app: express.Router) => {
     app.use("/pets", petRouter);
     app.use("/adotantes", adotanteRouter);
+    app.use("/abrigos", abrigoRouter);
 };
 
 export default router;
